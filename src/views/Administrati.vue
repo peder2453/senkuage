6d16ec093940e2cb42da0c7dde2f0d284d6362b6
peder16 <peder16@qq.com>
2020-12-29 23:23:42 +0800

    静态页面完成

--- src/views/Administrati.vue
+++ src/views/Administrati.vue
@@ -31,6 +31,74 @@
       </li>
     </div>
   </div>
+
+  <div class="trade">
+    <div class="conent">
+      <div class="tit">TRADE WITH EDGE</div>
+      <div class="cont">
+        Market Signals and Analytics Crypto arbitrage with orderbook depth
+        check. Explosive movers in Crypto.Company under/over valuation.
+        Automated options chain pricing evaluation and more...
+      </div>
+    </div>
+  </div>
+
+  <div class="defi">
+    <div class="tit">Senkuage Defi Platform Works</div>
+    <div class="cont">
+      <img src="@/assets/administrati/defiimg.png" alt="" class="leftimg" />
+      <div class="right-cont">
+        <div class="tit">Senkuage Arbitrage Bots Profits</div>
+        <div class="r-cont">
+          Senkuage Arbitrage Bots <br />
+          Senkuage Arbitrage Bots is a savvy arbitrage hunters that know various
+          ways to capitalise on opportunities. <br />
+          1 Simple arbitrageurs <br />
+          2 Fiat traiangular arbitrage <br />
+          3 Crypto Triangular arbitrage <br />
+          4 Convergence arbitrage <br />
+          5 Cash-and-Carry arbitrage
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="works">
+    <div class="tit">Senkuage Defi Platform Works</div>
+    <li>
+      <div class="li-left">
+        <div class="li-tit">DEPOSIT ASSET</div>
+        <div class="cont">
+          Investors can deposit selected supported <br />
+          currencies at anytime and anywhere <br />
+          Collateral remains on-chain for <br />
+          the duration of trading
+        </div>
+      </div>
+      <img src="@/assets/administrati/works0.png" alt="" class="right-img" />
+    </li>
+    <li class="reve">
+      <div class="li-left">
+        <div class="li-tit">PICK YOUR</div>
+        <div class="cont">
+          Easy to choose available Defi packages <br />
+          to profitAll asset transaction is <br />
+          on-chain, safe and secure
+        </div>
+      </div>
+      <img src="@/assets/administrati/works1.png" alt="" class="right-img" />
+    </li>
+    <li>
+      <div class="li-left">
+        <div class="li-tit">PROFIT AND EXIT</div>
+        <div class="cont">
+          Always deliver your profit on time and secure. <br />
+          Can exit the platform with an ease of use interface
+        </div>
+      </div>
+      <img src="@/assets/administrati/works2.png" alt="" class="right-img" />
+    </li>
+  </div>
 </template>
 
 <script>
@@ -104,6 +172,105 @@ export default {
         color: #2c79f0;
         font-weight: initial;
       }
+    }
+  }
+}
+.trade {
+  height: 13rem;
+  background: url("~@/assets/administrati/back1.png") no-repeat center/cover;
+  position: relative;
+  .conent {
+    position: absolute;
+    right: 0;
+    top: 0;
+    height: 100%;
+    width: 40%;
+    color: #fff;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    padding-right: 10%;
+    .tit {
+      font-size: 1.3rem;
+      font-weight: bold;
+      margin-bottom: 0.5rem;
+    }
+    .cont {
+      line-height: 1.5;
+      font-size: 0.6rem;
+    }
+  }
+}
+.defi {
+  & > .tit {
+    text-align: center;
+    margin-top: 2rem;
+    font-weight: bold;
+    font-size: 1rem;
+  }
+  .cont {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    img {
+      width: 13rem;
+      margin-right: 1rem;
+    }
+    .right-cont {
+      width: 500px;
+      padding-top: 1rem;
+      .tit {
+        color: #2c79f0;
+        font-size: 1.2rem;
+        text-align: left;
+        font-weight: 700;
+        margin-bottom: 0.6rem;
+      }
+      .r-cont {
+        color: #666666;
+        font-size: 0.5rem;
+      }
+    }
+  }
+}
+.works {
+  & > .tit {
+    text-align: center;
+    margin-top: 2rem;
+    font-weight: bold;
+    font-size: 1rem;
+  }
+  li {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    margin: 2rem 3rem;
+    &.reve {
+      flex-direction: row-reverse;
+    }
+    .li-left {
+      width: 500px;
+      padding-top: 1rem;
+      margin-bottom: 1rem;
+      .li-tit {
+        color: #2c79f0;
+        font-size: 1.2rem;
+        text-align: left;
+        font-weight: 700;
+        margin-bottom: 0.3rem;
+      }
+      .cont {
+        color: #7cabf3;
+        font-size: 0.6rem;
+        line-height: 1.5;
+      }
+    }
+    .right-img {
+      width: 13rem;
+      margin: 0 2rem;
     }
   }
 }
